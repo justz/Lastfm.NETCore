f7db35bbde9bb1be867fb66d4aecd8b1e6d6ed96
Dmitry Osin <d@osin.pw>
2017-08-10 20:18:13 +0300

    Added ErrorResponse & RestClientException

---
+++ Lastfm.NETCore/Common/ErrorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Lastfm.NETCore.Common
+{
+    public class ErrorResponse
+    {
+        public string Message { get; set; }
+        public string Error { get; set; }
+    }
+}
---
+++ Lastfm.NETCore/Common/RestClientException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lastfm.NETCore.Common
+{
+    public class RestClientException : Exception
+    {
+        public ErrorResponse ErrorResponse { get; }
+
+        public RestClientException()
+        {
+        }
+
+        public RestClientException(string message) : base(message)
+        {
+        }
+
+        public RestClientException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public RestClientException(ErrorResponse errorResponse, Exception innerException)
+        {
+            ErrorResponse = errorResponse;
+        }
+    }
+}
