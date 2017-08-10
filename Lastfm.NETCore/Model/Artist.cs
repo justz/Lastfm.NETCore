ab81801fdf2429d944b070b2392848b6b7ea015d
Dmitry Osin <d@osin.pw>
2017-08-10 22:39:03 +0300

    Artist fixes

--- Lastfm.NETCore/Model/Artist.cs
+++ Lastfm.NETCore/Model/Artist.cs
@@ -1,7 +1,29 @@
-﻿namespace Lastfm.NETCore.Model
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Lastfm.NETCore.Model
 {
     public class Artist
     {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("mbid")]
+        public string Mbid { get; set; }
+
+        [JsonProperty("match")]
+        public string Match { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("image")]
+        public IList<Image> Image { get; set; }
+
+        [JsonProperty("streamable")]
+        public string Streamable { get; set; }
         
+        [JsonProperty("listeners")]
+        public string Listeners { get; set; }
     }
 }
