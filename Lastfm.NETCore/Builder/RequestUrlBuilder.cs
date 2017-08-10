c1b7df63b7fe83679e0edec3f1600659570f0c15
Dmitry Osin <d@osin.pw>
2017-08-10 20:05:48 +0300

    Added RequestUrlBuilder

---
+++ Lastfm.NETCore/Builder/RequestUrlBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace Lastfm.NETCore.Builder
+{
+    public class RequestUrlBuilder
+    {
+        private readonly StringBuilder _builder = new StringBuilder();
+
+        public RequestUrlBuilder()
+        {
+            _builder.Append(BaseUrl);
+        }
+
+        public RequestUrlBuilder SetFormat(string format = "json")
+        {
+            _builder.Append($"&format={format}");
+            return this;
+        }
+
+        public RequestUrlBuilder SetAutoCorrect(int autocorrect)
+        {
+            _builder.Append($"&autocorrect={autocorrect}");
+            return this;
+        }
+
+        public RequestUrlBuilder SetLimit(int limit)
+        {
+            _builder.Append($"&limit={limit}");
+            return this;
+        }
+
+        public RequestUrlBuilder SetMethod(string method)
+        {
+            _builder.Append($"&method={method}");
+            return this;
+        }
+
+        public RequestUrlBuilder SetApiKey(string key)
+        {
+            _builder.Append($"&api_key={key}");
+            return this;
+        }
+
+        public RequestUrlBuilder SetExtraMethod(string extra)
+        {
+            _builder.Append($"&{extra}");
+            return this;
+        }
+
+        public string Build()
+        {
+            return _builder.ToString();
+        }
+
+        public const string BaseUrl = "http://ws.audioscrobbler.com/2.0/?";
+    }
+}
