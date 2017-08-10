2a2bfe9ad53cb6fc16f2b07555a853bd7e575cbe
Dmitry Osin <d@osin.pw>
2017-08-11 01:39:22 +0300

    Declare and use GetRequest<T> method

--- Lastfm.NETCore/Model/Artist.cs
+++ Lastfm.NETCore/Model/Artist.cs
@@ -42,175 +42,74 @@ namespace Lastfm.NETCore.Model
 
         public static async Task<Artist> GetInfoAsync(string name)
         {
-            using (var client = new HttpClient())
-            {
-                var url = new RequestUrlBuilder()
-                    .SetMethod("artist.getInfo")
-                    .SetExtraMethod($"artist={name}")
-                    .SetAutoCorrect(true)
-                    .SetApiKey(ApiKeyProvider.Instance.ApiKey)
-                    .SetFormat()
-                    .Build();
+            var url = new RequestUrlBuilder()
+                .SetMethod("artist.getInfo")
+                .SetExtraMethod($"artist={name}")
+                .SetAutoCorrect(true)
+                .SetApiKey(ApiKeyProvider.Instance.ApiKey)
+                .SetFormat()
+                .Build();
 
-                string content = null;
-
-                try
-                {
-                    var response = await client.GetAsync(url).ConfigureAwait(false);
-                    content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-                    response.EnsureSuccessStatusCode();
-
-                    var artist = await RestClientHelper
-                        .ParseResponse<Artist>(JObject.Parse(content)["artist"].ToString())
-                        .ConfigureAwait(false);
-
-                    await ThrowIfNull(artist, content);
-
-                    return artist;
-                }
-                catch (Exception ex)
-                {
-                    throw RestClientHelper.GetException(ex, content);
-                }
-            }
+            var artist = await GetRequest<Artist>(url, o => o["artist"]);
+            return artist;
         }
 
         public static async Task<List<Artist>> SearchAsync(string name, int count = 10)
         {
-            using (var client = new HttpClient())
-            {
-                var url = new RequestUrlBuilder()
-                    .SetMethod("artist.search")
-                    .SetExtraMethod($"artist={name}")
-                    .SetLimit(count)
-                    .SetAutoCorrect(true)
-                    .SetApiKey(ApiKeyProvider.Instance.ApiKey)
-                    .SetFormat()
-                    .Build();
+            var url = new RequestUrlBuilder()
+                .SetMethod("artist.search")
+                .SetExtraMethod($"artist={name}")
+                .SetLimit(count)
+                .SetAutoCorrect(true)
+                .SetApiKey(ApiKeyProvider.Instance.ApiKey)
+                .SetFormat()
+                .Build();
 
-                string content = null;
-
-                try
-                {
-                    var response = await client.GetAsync(url).ConfigureAwait(false);
-                    content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-                    response.EnsureSuccessStatusCode();
-
-                    var artists = await RestClientHelper.ParseResponse<List<Artist>>(
-                        JObject.Parse(content)["results"]["artistmatches"]["artist"].ToString()
-                    ).ConfigureAwait(false);
-
-                    await ThrowIfNull(artists, content);
-                    return artists;
-                }
-                catch (Exception ex)
-                {
-                    throw RestClientHelper.GetException(ex, content);
-                }
-            }
+            var artists = await GetRequest<List<Artist>>(url, o => o["results"]["artistmatches"]["artist"]);
+            return artists;
         }
         
         public static async Task<Artist> GetCorrectionAsync(string name)
         {
-            using (var client = new HttpClient())
-            {
-                var url = new RequestUrlBuilder()
-                    .SetMethod("artist.getCorrection")
-                    .SetExtraMethod($"artist={name}")
-                    .SetApiKey(ApiKeyProvider.Instance.ApiKey)
-                    .SetFormat()
-                    .Build();
+            var url = new RequestUrlBuilder()
+                .SetMethod("artist.getCorrection")
+                .SetExtraMethod($"artist={name}")
+                .SetApiKey(ApiKeyProvider.Instance.ApiKey)
+                .SetFormat()
+                .Build();
 
-                string content = null;
-
-                try
-                {
-                    var response = await client.GetAsync(url).ConfigureAwait(false);
-                    content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-                    response.EnsureSuccessStatusCode();
-
-                    var artist = await RestClientHelper.ParseResponse<Artist>(JObject.Parse(content)["corrections"]["correction"]["artist"].ToString()).ConfigureAwait(false);
-
-                    await ThrowIfNull(artist, content);
-                    return artist;
-                }
-                catch (Exception ex)
-                {
-                    throw RestClientHelper.GetException(ex, content);
-                }
-            }
+            var artist = await GetRequest<Artist>(url, o => o["corrections"]["correction"]["artist"]);
+            return artist;
         }
         
         public static async Task<List<Artist>> GetSimilarAsync(string name, int count = 10)
         {
-            using (var client = new HttpClient())
-            {
-                var url = new RequestUrlBuilder()
-                    .SetMethod("artist.getSimilar")
-                    .SetExtraMethod($"artist={name}")
-                    .SetAutoCorrect(true)
-                    .SetLimit(count)
-                    .SetApiKey(ApiKeyProvider.Instance.ApiKey)
-                    .SetFormat()
-                    .Build();
+            var url = new RequestUrlBuilder()
+                .SetMethod("artist.getSimilar")
+                .SetExtraMethod($"artist={name}")
+                .SetAutoCorrect(true)
+                .SetLimit(count)
+                .SetApiKey(ApiKeyProvider.Instance.ApiKey)
+                .SetFormat()
+                .Build();
 
-                string content = null;
-
-                try
-                {
-                    var response = await client.GetAsync(url).ConfigureAwait(false);
-                    content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-                    response.EnsureSuccessStatusCode();
-
-                    var artists = await RestClientHelper.ParseResponse<List<Artist>>(JObject.Parse(content)["similarartists"]["artist"].ToString()).ConfigureAwait(false);
-
-                    await ThrowIfNull(artists, content);
-                    return artists;
-                }
-                catch (Exception ex)
-                {
-                    throw RestClientHelper.GetException(ex, content);
-                }
-            }
+            var artists = await GetRequest<List<Artist>>(url, o => o["similarartists"]["artist"]);
+            return artists;
         }
         
         public static async Task<IEnumerable<Track>> GetTopTracksAsync(string name, int count = 20)
         {
-            using (var client = new HttpClient())
-            {
-                var url = new RequestUrlBuilder()
-                    .SetMethod("artist.getTopTracks")
-                    .SetExtraMethod($"artist={name}")
-                    .SetLimit(count)
-                    .SetAutoCorrect(true)
-                    .SetApiKey(ApiKeyProvider.Instance.ApiKey)
-                    .SetFormat()
-                    .Build();
+            var url = new RequestUrlBuilder()
+                .SetMethod("artist.getTopTracks")
+                .SetExtraMethod($"artist={name}")
+                .SetLimit(count)
+                .SetAutoCorrect(true)
+                .SetApiKey(ApiKeyProvider.Instance.ApiKey)
+                .SetFormat()
+                .Build();
 
-                string content = null;
-                
-                try
-                {
-                    var response = await client.GetAsync(url).ConfigureAwait(false);
-                    content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-                    response.EnsureSuccessStatusCode();
-
-                    var tracks = await RestClientHelper.ParseResponse<List<Track>>(JObject.Parse(content)["toptracks"]["track"].ToString()).ConfigureAwait(false);
-
-                    await ThrowIfNull(tracks, content);
-                    
-                    return tracks;
-                }
-                catch (Exception ex)
-                {
-                    throw RestClientHelper.GetException(ex, content);
-                }
-            }
+            var tracks = await GetRequest<List<Track>>(url, o => o["toptracks"]["track"]);
+            return tracks;
         }
         
         public static async Task<List<Album>> GetTopAlbumsAsync(string name, int count = 10)
