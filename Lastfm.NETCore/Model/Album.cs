f7b6215f0612a0039b31e2d7af9fbca938a16708
Dmitry Osin <d@osin.pw>
2017-08-10 22:34:51 +0300

    Added Album model

--- Lastfm.NETCore/Model/Album.cs
+++ Lastfm.NETCore/Model/Album.cs
@@ -1,7 +1,26 @@
-﻿namespace Lastfm.NETCore.Model
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Lastfm.NETCore.Model
 {
     public class Album
     {
-        
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("playcount")]
+        public int Playcount { get; set; }
+
+        [JsonProperty("mbid")]
+        public string Mbid { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("artist")]
+        public Artist Artist { get; set; }
+
+        [JsonProperty("image")]
+        public IList<Image> Image { get; set; }
     }
 }
