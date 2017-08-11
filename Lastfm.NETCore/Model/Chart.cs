a32d472e30eb248b8697b8738aa578ab91aa8b80
Dmitry Osin <d@osin.pw>
2017-08-11 17:27:17 +0300

    GetTopArtists impl

---
+++ Lastfm.NETCore/Model/Chart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lastfm.NETCore.Builder;
+using Lastfm.NETCore.Common;
+using Lastfm.NETCore.Helper;
+
+namespace Lastfm.NETCore.Model
+{
+    public class Chart
+    {
+        public static async Task<List<Artist>> GetTopArtists()
+        {
+            var url = new RequestUrlBuilder()
+                .SetApiKey(ApiKeyProvider.Instance.ApiKey)
+                .SetMethod("chart.gettopartists")
+                .SetFormat()
+                .Build();
+
+            var artists = await RestClientHelper.GetRequest<List<Artist>>(url, o => o["artists"]["artist"]);
+            return artists;
+        }
+    }
+}
---
+++ Lastfm.Tests/ChartTests.cs
@@ -0,0 +1,14 @@
+﻿using Lastfm.NETCore.Model;
+using Xunit;
+
+namespace Lastfm.Tests
+{
+    public class ChartTests
+    {
+        [Fact]
+        public async void GetTopArtists_Test()
+        {
+            var artists = await Chart.GetTopArtists();
+        }
+    }
+}
